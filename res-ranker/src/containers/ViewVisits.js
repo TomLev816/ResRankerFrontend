5e8e7c5a17467cf4bfa54ab109f5d3e6ddd43c69
github username <github email address>
2018-12-04 11:40:00 -0500

    styled visit page

--- res-ranker/src/containers/ViewVisits.js
+++ res-ranker/src/containers/ViewVisits.js
@@ -1,38 +1,52 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { userLoggedInAction } from '../store/actions/'
-
-
+// import Swiper from 'react-id-swiper';
+//
+//
+// const params = {
+//    slidesPerView: 3,
+//    spaceBetween: 30,
+//    pagination: {
+//      el: '.swiper-pagination',
+//      clickable: false,
+//    }
+//  };
 
 
 function ViewVisits(props) {
+
 
 
 const renderVisits = (props) => {
   const {allVisits, userLoggedIn, rankedRestaurants, allRestaurants} = props
   let userVisits = allVisits.filter(visit => visit.user_id === userLoggedIn.id)
 
-  return userVisits.map(visit => {
+return (
+   userVisits.map(visit => {
     let restaurant = rankedRestaurants.find(restaurant => restaurant.restaurant_id === visit.restaurant_id)
     let restaurantInfo = allRestaurants.find(rest => rest.id === restaurant.restaurant_id)
+    console.log(visit);
     return (
-      <div className='user-show-restaurant-visit' >
-        <div className='photo-of-restaurant'>
-          <img src={visit.image_url} alt="" ></img>
-        </div>
-        <div className='info-on-visit'>
+      <div className='slider-holder' >
+        <img className="members-image" src={visit.image_url} alt="" ></img>
+        <div className=''>
           <h1>{restaurantInfo.name}</h1>
-          <h3>Date of Visit: {visit.date.slice(0, 10)}</h3>
+          <h2>Meal: {visit.meal_eaten}</h2>
+          <h3>Date of Visit: {visit.date.slice(5, 10)}-{visit.date.slice(0, 4) }</h3>
           <h3>Comment: {visit.comment}</h3>
         </div>
       </div>
     )
   })
+  )
 }
 
   return (
     <div className='visits-page'>
-      {renderVisits(props)}
+
+        {renderVisits(props)}
+
     </div>
   );
 }
